b2fad07b9b95c5a7059e0e3af97e160daf2c2979
Ferry Ayunda <114124884+susahberak@users.noreply.github.com>
2022-10-27 20:34:45 +0700

    Update paste.php

--- paste.php
+++ paste.php
@@ -92,7 +92,7 @@ if (isset($_POST['text'])) {
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
             </svg>
             by
-            <a href="https://www.instagram.com/dewana_kl/">DewanaKL</a>
+            <a href="#">Ferry Ayunda</a>
         </p>
     </footer>
     <script>
